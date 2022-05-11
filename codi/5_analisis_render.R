57c41f0bd43309a5840977f5b2b9f137e077a1fb
Ramon Puig Treserres <raypuig@gmail.com>
2022-05-11 14:00:36 +0200

    act

--- codi/5_analisis_render.R
+++ codi/5_analisis_render.R
@@ -37,83 +37,43 @@
 #                           2018                                                                   #
 #--------------------------------------------------------------------------------------------------#
 #---------------------------- LECTURA1 ------------------------------------------------------------# 
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
+#library("dplyr")
+#rm(list=ls())
+#gc()
+#mostra=T
+#dir_dades_desti="dades" 
+#dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
 
 
-if (mostra) {
-  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
-  dir_dades_desti="dades/mostra" }
+#if (mostra) {
+#  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
+#  dir_dades_desti="dades/mostra" }
 
 #--------------------------------------------------------------------------------------------------# 
-rmarkdown::render(input="./codi/1_lectura1.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Lectura1_"),Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
+#rmarkdown::render(input="./codi/1_lectura1.Rmd",
+#                  output_file=paste0(here::here("outputs/Informe_Lectura1_"),Sys.Date()),
+#                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
 #--------------------------------------------------------------------------------------------------#
 
 #---------------------------- LECTURA2 ------------------------------------------------------------# 
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
-if (mostra) {
-  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
-  dir_dades_desti="dades/mostra" }
+#library("dplyr")
+#rm(list=ls())
+#gc()
+#mostra=T
+#dir_dades_desti="dades" 
+#dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
+#if (mostra) {
+#  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
+#  dir_dades_desti="dades/mostra" }
 #--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/1_lectura2.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Lectura2_"),Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
-
+#rmarkdown::render(input="./codi/1_lectura2.Rmd",
+#                  output_file=paste0(here::here("outputs/Informe_Lectura2_"),Sys.Date()),
+#                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))#
+#
 #--------------------------------------------------------------------------------------------------#
 #../DADES/HTG/HTG" 
 
-#---------------------------  PREPARACIO ----------------------------------------------------------#
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
-if (mostra) {
-  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
-  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-ANY="20181231"
-#--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/2_preparacio.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Preparacio_"),Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti))
 
-#--------------------------------------------------------------------------------------------------#
-
-#---------------------------  ANALISI -------------------------------------------------------------#
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-if (mostra) {
-  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-#--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/3_analisis.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Exploratori_Final_epiTempus_DEBUT_DM2_rai"),"_",Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti))
-#--------------------------------------------------------------------------------------------------#
-
-
-
-
-
-
-
-#
 
 
 
