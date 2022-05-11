19faabd2df929ae1554c50b0f1fe51bd2f9ebd38
Ramon Puig Treserres <raypuig@gmail.com>
2022-05-11 13:54:59 +0200

    act

--- codi/5_analisis_render.R
+++ codi/5_analisis_render.R
@@ -1,5 +1,5 @@
 #--------------------------------------------------------------------------------------------------#
-# 29.04.2022  #
+# 11.05.2022  #
 #--------------------------------------------------------------------------------------------------#
 #Debut de la  Diabetis Mellitus TIPUS 2 a Catalunya.[2007-2018] 
 
@@ -71,6 +71,50 @@ rmarkdown::render(input="./codi/1_lectura2.Rmd",
                   params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
 
 #--------------------------------------------------------------------------------------------------#
+#../DADES/HTG/HTG" 
+
+#---------------------------  PREPARACIO ----------------------------------------------------------#
+library("dplyr")
+rm(list=ls())
+gc()
+mostra=T
+dir_dades_desti="dades" 
+dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
+if (mostra) {
+  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
+  dir_dades_desti="dades/mostra" }
+#--------------------------------------------------------------------------------------------------#
+ANY="20181231"
+#--------------------------------------------------------------------------------------------------#
+rmarkdown::render(input="./codi/2_preparacio.Rmd",
+                  output_file=paste0(here::here("outputs/Informe_Preparacio_"),Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti))
+
+#--------------------------------------------------------------------------------------------------#
+
+#---------------------------  ANALISI -------------------------------------------------------------#
+library("dplyr")
+rm(list=ls())
+gc()
+mostra=T
+dir_dades_desti="dades" 
+if (mostra) {
+  dir_dades_desti="dades/mostra" }
+#--------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------#
+rmarkdown::render(input="./codi/3_analisis.Rmd",
+                  output_file=paste0(here::here("outputs/Informe_Exploratori_Final_epiTempus_DEBUT_DM2_rai"),"_",Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti))
+#--------------------------------------------------------------------------------------------------#
+
+
+
+
+
+
+
+#
+
 
 
 
@@ -130,7 +174,7 @@ if (mostra) {
 #--------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------#
 rmarkdown::render(input="./codi/3_analisis.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Exploratori_Final_epiTempus_DEBUT_DM2"),"_",Sys.Date()),
+                  output_file=paste0(here::here("outputs/Informe_Exploratori_Final_epiTempus_DEBUT_DM2_jordi"),"_",Sys.Date()),
                   params = list(dir_dades_desti=dir_dades_desti))
 #--------------------------------------------------------------------------------------------------#
 
