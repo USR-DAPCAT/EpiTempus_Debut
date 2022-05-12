94a122a7377467bedef34bdfd4a448539a4b0000
Ramon Puig Treserres <raypuig@gmail.com>
2022-05-12 17:11:14 +0200

    ACT

--- codi/5_analisis_render.R
+++ codi/5_analisis_render.R
@@ -79,12 +79,17 @@
 
 
 
-#now
 
-
-
-
-
+###############################################################################################################
+#
+#                                    ##################
+#                                    #  M  O S T R A  #
+#                                    ##################
+#
+#
+#
+#
+#
 #---------------------------- LECTURA_GRAN_FUNCIO ------------------------------------------------------------# 
 library("dplyr")
 rm(list=ls())
@@ -140,10 +145,69 @@ rmarkdown::render(input="./codi/3_analisis.Rmd",
 
 
 
+
+
+###############################################################################################################
 #
+#                                                  ##################
+#                                                  #P  O B L A C I O#
+#                                                  ##################
+#
+#
+#---------------------------- LECTURA_GRAN_FUNCIO ------------------------------------------------------------# 
+library("dplyr")
+rm(list=ls())
+gc()
+mostra=FALSE
+dir_dades_desti="dades" 
+dir_dades_origen="../DADES/EPIPEU_CAT3/dades" 
 
+if (mostra) {
+  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
+  dir_dades_desti="dades/mostra" }
+#--------------------------------------------------------------------------------------------------#
+rmarkdown::render(input="./codi/1_lectura_Gran_Funcio.Rmd",
+                  output_file=paste0(here::here("outputs/Informe_Lectura_Gran_Funcio"),Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
 
+#--------------------------------------------------------------------------------------------------#
 
+#../DADES/HTG/HTG" 
+
+#---------------------------  PREPARACIO ----------------------------------------------------------#
+library("dplyr")
+rm(list=ls())
+gc()
+mostra=FALSE
+dir_dades_desti="dades" 
+dir_dades_origen="../DADES/EPIPEU_CAT3/dades" 
+
+if (mostra) {
+  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
+  dir_dades_desti="dades/mostra" }
+#--------------------------------------------------------------------------------------------------#
+ANY="20181231"
+#--------------------------------------------------------------------------------------------------#
+rmarkdown::render(input="./codi/2_preparacio.Rmd",
+                  output_file=paste0(here::here("outputs/Informe_Preparacio_"),Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti))
+
+#--------------------------------------------------------------------------------------------------#
+
+#---------------------------  ANALISI -------------------------------------------------------------#
+library("dplyr")
+rm(list=ls())
+gc()
+mostra=FALSE
+dir_dades_desti="dades" 
+if (mostra) {
+  dir_dades_desti="dades/mostra" }
+#--------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------#
+rmarkdown::render(input="./codi/3_analisis.Rmd",
+                  output_file=paste0(here::here("outputs/Informe_Exploratori_Final_epiTempus_DEBUT_DM2_jordi"),"_",Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti))
+#--------------------------------------------------------------------------------------------------#
 
 
 
