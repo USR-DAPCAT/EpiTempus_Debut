bb668ec5f9658eb0fe668ce05c6c507f3acfabf2
jrealgatius <jordireal@gmail.com>
2022-05-24 12:30:39 +0200

    Actualització. Neteja

--- codi/5_analisis_render.R
+++ codi/5_analisis_render.R
@@ -1,154 +1,3 @@
-#--------------------------------------------------------------------------------------------------#
-# 12.05.2022  #
-#--------------------------------------------------------------------------------------------------#
-#Debut de la  Diabetis Mellitus TIPUS 2 a Catalunya.[2007-2018] 
-
-#TRASPLANTAMENT DE RONYÓ=Z94.0
-
-
-#[12.04.2022]
-
-###################################################################
-########################  MOSTRA  #################################
-###################################################################
-
-#require(devtools)
-#devtools::install_github("benjaminrich/table1")
-
-
-#--------------------------------------------------------------------------------------------------#
-#Adjunto el Excel, 
-#he cambiado el nombre del Excel para evitar confusiones,  
-#he reducido el numero de agrupadores,  están todos los que habían antes en el AGR1
-#Mas importante, 
-#el criterio de inclusion para el estudio de Carlos esta en agrupador DM2_inclusion 
-#( diagnostico y/o tratamiento antidiabético) falta solo glicada HbA1c>6.5%
-#Nuevo proyecto nuevo orden
-#--------------------------------------------------------------------------------------------------#
-
-
-
-#[entrada>=20080101 & sortida<=20181231]
-
-#before
-
-
-#--------------------------------------------------------------------------------------------------#
-#                           2018                                                                   #
-#--------------------------------------------------------------------------------------------------#
-#---------------------------- LECTURA1 ------------------------------------------------------------# 
-#library("dplyr")
-#rm(list=ls())
-#gc()
-#mostra=T
-#dir_dades_desti="dades" 
-#dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
-
-
-#if (mostra) {
-#  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
-#  dir_dades_desti="dades/mostra" }
-
-#--------------------------------------------------------------------------------------------------# 
-#rmarkdown::render(input="./codi/1_lectura1.Rmd",
-#                  output_file=paste0(here::here("outputs/Informe_Lectura1_"),Sys.Date()),
-#                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
-#--------------------------------------------------------------------------------------------------#
-
-#---------------------------- LECTURA2 ------------------------------------------------------------# 
-#library("dplyr")
-#rm(list=ls())
-#gc()
-#mostra=T
-#dir_dades_desti="dades" 
-#dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
-#if (mostra) {
-#  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
-#  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-#rmarkdown::render(input="./codi/1_lectura2.Rmd",
-#                  output_file=paste0(here::here("outputs/Informe_Lectura2_"),Sys.Date()),
-#                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))#
-#
-#--------------------------------------------------------------------------------------------------#
-#../DADES/HTG/HTG" 
-
-
-
-
-
-
-
-
-###############################################################################################################
-#
-#                                    ##################
-#                                    #  M  O S T R A  #
-#                                    ##################
-#
-#
-#
-#
-#
-#---------------------------- LECTURA_GRAN_FUNCIO ------------------------------------------------------------# 
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-dir_dades_origen="../DADES/EPIPEU_CAT3/dades" 
-if (mostra) {
-  dir_dades_origen="../DADES/EPIPEU_CAT3/dades/mostra"
-  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/1_lectura_Gran_Funcio.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Lectura_Gran_Funcio"),Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
-
-#--------------------------------------------------------------------------------------------------#
-
-#../DADES/HTG/HTG" 
-
-#---------------------------  PREPARACIO ----------------------------------------------------------#
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-dir_dades_origen="../DADES/EPIPEU_CAT3/dades" 
-if (mostra) {
-  dir_dades_origen="../DADES/EPIPEU_CAT3/dades/mostra"
-  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-ANY="20181231"
-#--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/2_preparacio.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Preparacio_"),Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti))
-
-#--------------------------------------------------------------------------------------------------#
-
-#---------------------------  ANALISI -------------------------------------------------------------#
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-if (mostra) {
-  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-#--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/3_analisis.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Exploratori_Final_epiTempus_DEBUT_DM2_jordi"),"_",Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti))
-#--------------------------------------------------------------------------------------------------#
-
-
-
-
-
-###############################################################################################################
-#
 #                                                  ##################
 #                                                  #P  O B L A C I O#
 #                                                  ##################
@@ -165,6 +14,8 @@ dir_dades_origen="../DADES/EPIPEU_CAT3/dades"
 if (mostra) {
   dir_dades_origen="../DADES/EPIPEU_CAT3/dades/mostra"
   dir_dades_desti="dades/mostra" }
+
+
 #--------------------------------------------------------------------------------------------------#
 rmarkdown::render(input="./codi/1_lectura_Gran_Funcio.Rmd",
                   output_file=paste0(here::here("outputs/Informe_Lectura_Gran_Funcio"),Sys.Date()),
@@ -185,14 +36,12 @@ dir_dades_origen="../DADES/EPIPEU_CAT3/dades"
 if (mostra) {
   dir_dades_origen="../DADES/EPIPEU_CAT3/dades/mostra"
   dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-# ANY="20181231"
+
 #--------------------------------------------------------------------------------------------------#
 rmarkdown::render(input="./codi/2_preparacio.Rmd",
                   output_file=paste0(here::here("outputs/Informe_Preparacio_"),Sys.Date()),
                   params = list(dir_dades_desti=dir_dades_desti))
 
-#--------------------------------------------------------------------------------------------------#
 
 #---------------------------  ANALISI -------------------------------------------------------------#
 library("dplyr")
@@ -205,77 +54,7 @@ if (mostra) {
 #--------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------#
 rmarkdown::render(input="./codi/3_analisis.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Exploratori_Final_epiTempus_DEBUT_DM2_POBLACIO"),"_",Sys.Date()),
+                  output_file=paste0(here::here("outputs/Informe_Exploratori_epiTempus_DEBUTDM2_POBLACIO"),"_",Sys.Date()),
                   params = list(dir_dades_desti=dir_dades_desti))
 #--------------------------------------------------------------------------------------------------#
-
-
-#--------------------------------------------------------------------------------------------------#
-#rai :: 18.05.2022
-#--------------------------------------------------------------------------------------------------#
-
-###############################################################################################################
-#
-#                                    ##################
-#                                    #  M  O S T R A  #
-#                                    ##################
-#
-#
-#
-#
-#
-#---------------------------- LECTURA_GRAN_FUNCIO ------------------------------------------------------------# 
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
-if (mostra) {
-  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
-  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/1_lectura_Gran_Funcio.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Lectura_Gran_Funcio"),Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
-
-#--------------------------------------------------------------------------------------------------#
-
-#../DADES/HTG/HTG" 
-
-#---------------------------  PREPARACIO ----------------------------------------------------------#
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-dir_dades_origen="../../DADES/EPIPEU_CAT3/dades" 
-if (mostra) {
-  dir_dades_origen="../../DADES/EPIPEU_CAT3/dades/mostra"
-  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-ANY="20181231"
-#--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/2_preparacio.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Preparacio_"),Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti))
-
-#--------------------------------------------------------------------------------------------------#
-
-#---------------------------  ANALISI -------------------------------------------------------------#
-library("dplyr")
-rm(list=ls())
-gc()
-mostra=T
-dir_dades_desti="dades" 
-if (mostra) {
-  dir_dades_desti="dades/mostra" }
-#--------------------------------------------------------------------------------------------------#
-#--------------------------------------------------------------------------------------------------#
-rmarkdown::render(input="./codi/3_analisis.Rmd",
-                  output_file=paste0(here::here("outputs/Informe_Exploratori_Final_epiTempus_DEBUT_DM2_Rai_"),"_",Sys.Date()),
-                  params = list(dir_dades_desti=dir_dades_desti))
-#--------------------------------------------------------------------------------------------------#
-
-
 
